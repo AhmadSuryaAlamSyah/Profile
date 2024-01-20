8e9ad0d3b3316bb74a6d2f430869dca0b22f3321
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2024-01-21 00:09:05 +0700

    update 10

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projek from './component/projek';
 import LoadingCv from './moal/LoadingCv';
 import { GlobalContext } from './global/GlobalContetxt';
 import { useContext } from 'react';
-
+import Footer from './component/footer';
 
 const App = () => {
   const { showCv, setShowCv } = useContext(GlobalContext);
@@ -17,6 +17,7 @@ const App = () => {
       <Section />
       <Projek />
       {showCv && <LoadingCv showCv={showCv} setShowCv={setShowCv} />}
+      <Footer />
     </div>
   );
 };
